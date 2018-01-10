9106a0860189d6cf0e1b2eefabe800e074f4f2bb
Firesword12 <karl.orul@mail.ee>
2018-01-10 10:39:29 +0200

    l

---
+++ WindowsFormsApp2/Mõtlemiskoht.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class Mõtlemiskoht
+    {
+        a= Must(ala)
+        b= Valge(ala)
+        c= Nupp1(Must)
+        d= Nupp2(Valge)
+    }
+}
