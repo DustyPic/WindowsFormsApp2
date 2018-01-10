620d1f25a804788fba742509bbd813cd01607cf2
Firesword12 <karl.orul@mail.ee>
2018-01-10 10:34:33 +0200

    Rules

---
+++ WindowsFormsApp2/Rules.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class Rules
+    {
+        /*Draughts or Checkers 
+        Equipment 
+          The game of Draughts is played on a standard Chess board 64 black and white chequered squares.Each player has 12 pieces normally in the form of fat round counters.
+          */
+
+        /*Preparation and Objective
+        Black always plays first. A coin is tossed to decide which player will be black.Each player's pieces are placed on the 12 black squares nearest to that player. 
+        */
+
+        /*Play
+        Players take turns to move a piece of their own colour.Any piece that reaches the far edge of the board is immediately crowned and is thereafter known as a "King".
+        */
+
+        /*Finishing
+        The game is won by the player who first manages to take all his opponent's pieces or renders them unable to move.
+        */
+
+        /*https://www.mastersgames.com/rules/draughts-rules.htm */
+    }
+}
